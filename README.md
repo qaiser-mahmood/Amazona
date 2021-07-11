2c827477b1061e32bd897eef4cc2a11e89b8df06
Qaiser Mahmood <hqmahmood@gmail.com>
2021-07-12 02:10:32 +0800

    Initial commit - Added project files and README.md to Git.

---
+++ README.md
@@ -0,0 +1,35 @@
+# amazona
+
+Amazona is an e-commerce website that has the functionality to display the products.
+* It also has a shopping cart functionality and payment options for Paypal and Stripe.
+* It also has an admin view, that has options related to admin user.
+* It uses Redux for state management.
+* Back-end is a node js server
+* It uses json web token api for authentication
+
+## Installation
+
+In the project folder run the following command to install dependencies to run Node js backend
+
+```bash
+npm install
+```
+
+In the frontend folder run the following command to install dependencies for React js
+```bash
+npm install
+```
+##### Make sure the mongodb service is running on your computer
+###### To check PayPal functionality, please provide your PayPal Client Id key in .env file in project folder
+## Usage
+In the project folder run the following command to run the backend server
+
+```bash
+npm run start
+```
+
+In the frontend folder run the following command to run the React js server
+
+```bash
+npm start
+```
---
+++ frontend/src/App.js
@@ -0,0 +1,114 @@
+import React from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { BrowserRouter, Link, Route } from "react-router-dom"
+import CartScreen from "./screens/CartScreen"
+import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
+import SigninScreen from "./screens/SigninScreen"
+import { signout } from "./actions/userActions"
+import RegisterScreen from "./screens/RegisterScreen"
+import ShippingAddressScreen from "./screens/ShippingAddressScreen"
+import PaymentMethodScreen from "./screens/PaymentMethodScreen"
+import PlaceOrderScreen from "./screens/PlaceOrderScreen"
+import OrderScreen from "./screens/OrderScreen"
+import OrderHistoryScreen from "./screens/OrderHistoryScreen"
+import ProfileScreen from "./screens/ProfileScreen"
+import PrivateRoute from "./components/PrivateRoute"
+
+function App() {
+  const cart = useSelector((state) => state.cart)
+  const { cartItems } = cart
+  const userSignin = useSelector((state) => state.userSignin)
+  const { userInfo } = userSignin
+  const dispatch = useDispatch()
+  const signoutHandler = () => {
+    dispatch(signout())
+  }
+
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link to="/" className="brand">
+              amazona
+            </Link>
+          </div>
+          <div>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/profile">User Profile</Link>
+                  </li>
+
+                  <li>
+                    <Link to="/orderhistory">Order History</Link>
+                  </li>
+
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <Link to="#admin">
+                  Admin <i className="fa fa-caret-down"></i>
+                </Link>
+
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                  </li>
+
+                  <li>
+                    <Link to="/productlist">Products</Link>
+                  </li>
+
+                  <li>
+                    <Link to="/orderlist">Orders</Link>
+                  </li>
+
+                  <li>
+                    <Link to="/userlist">Users</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </header>
+        <main>
+          <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/signin" component={SigninScreen} />
+          <Route path="/register" component={RegisterScreen} />
+          <Route path="/shipping" component={ShippingAddressScreen} />
+          <Route path="/payment" component={PaymentMethodScreen} />
+          <Route path="/placeorder" component={PlaceOrderScreen} />
+          <Route path="/order/:id" component={OrderScreen} />
+          <Route path="/orderhistory" component={OrderHistoryScreen} />
+          <PrivateRoute path="/profile" component={ProfileScreen} />
+          <Route path="/" component={HomeScreen} exact />
+        </main>
+        <footer className="row center">All rights reserved</footer>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
